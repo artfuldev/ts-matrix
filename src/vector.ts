7e4ce57a4be89a1a12dab0fab71824cf4554b0d6
Sudarsan Balaji <sudarsan.balaji@artfuldev.com>
2022-11-21 17:49:07 +0100

    Use consistent types

--- src/vector.ts
+++ src/vector.ts
@@ -1,19 +1,19 @@
 import { Nullable, Sum, Unwrap, TupleOf, Size } from "./tuple";
 
-export type Vector<A, S extends number> = TupleOf<A, S>;
+export type Vector<A, S extends number> = TupleOf<A, S> & { length: S };
 
 export const vector =
   <S extends number>(size: S) =>
   <A>(ras: readonly A[]): Nullable<Vector<A, S>> =>
     [ras]
       .filter((as) => as.length === size)
-      .map<Vector<A, S>>((as) => as as TupleOf<A, S>)
+      .map<Vector<A, S>>((as) => as as Vector<A, S>)
       .pop() ?? null;
 
 export const vec = <T extends any[]>(...as: T): Vector<Unwrap<T>, Size<T>> =>
-  as as TupleOf<Unwrap<T>, Size<T>>;
+  as as Vector<Unwrap<T>, Size<T>>;
 
 export const append =
   <A, T extends number>(vat: Vector<A, T>) =>
   <S extends number>(vas: Vector<A, S>): Vector<A, Sum<T, S>> =>
-    vas.concat(vat) as TupleOf<A, Sum<T, S>>;
+    vas.concat(vat) as Vector<A, Sum<T, S>>;
